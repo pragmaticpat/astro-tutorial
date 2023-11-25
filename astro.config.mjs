b72890b21be9d935dfc9be03b30c43103b59a886
pragmaticpat <pragmaticpat@me.com>
2023-11-25 16:14:26 -0500

    finished the tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify/functions";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  site: "https://astro-tutorial-pragmaticpat.netlify.app/"
+  site: "https://astro-tutorial-pragmaticpat.netlify.app/",
+  integrations: [preact()]
 });
